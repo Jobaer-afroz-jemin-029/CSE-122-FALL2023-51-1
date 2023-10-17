6a39dc376275a5c1dbb51463c8fbe046330b1071
Jobaer Afroz Jemin <139230529+Jobaer-afroz-jemin-029@users.noreply.github.com>
2023-10-17 16:31:03 +0600

    Create A. Tram.cpp

---
+++ lab-09/A. Tram.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n,in=0,out=0,max=0;
+cin>>n;
+for(int i=0;i<n;i++){
+int x,y;
+cin>>x>>y;
+in=in-x;
+if(max<in)
+max=in;
+in=in+y;
+if(max<in)
+max=in;
+}
+cout<<max<<endl;
+
+   return 0;
+}
